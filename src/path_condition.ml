0d6859d2844277635fdf6688a814f6e840f4b999
redianthus <leo@ocaml.pro>
2026-02-12 11:47:25 +0100

    prepare to extend the PC type

--- src/path_condition.ml
+++ src/path_condition.ml
@@ -4,28 +4,34 @@
 
 module Union_find = Union_find.Make (Smtml.Symbol)
 
-type t = Smtml.Expr.Set.t Union_find.t
+type union_find = Smtml.Expr.Set.t Union_find.t
 
-let pp : t Fmt.t = fun ppf pc -> (Union_find.pp Smtml.Expr.Set.pp) ppf pc
+type t = { union_find : union_find }
 
-let empty : t = Union_find.empty
+let pp : t Fmt.t =
+ fun ppf { union_find } -> (Union_find.pp Smtml.Expr.Set.pp) ppf union_find
 
-let add_one (condition : Smtml.Expr.t) pc : t =
+let empty : t =
+  let union_find = Union_find.empty in
+  { union_find }
+
+let add_one (condition : Smtml.Expr.t) (pc : t) : t =
   match Smtml.Expr.get_symbols [ condition ] with
   | hd :: tl ->
     (* We add the first symbol to the UF *)
-    let pc =
+    let union_find =
       let c = Smtml.Expr.Set.singleton condition in
-      Union_find.add ~merge:Smtml.Expr.Set.union hd c pc
+      Union_find.add ~merge:Smtml.Expr.Set.union hd c pc.union_find
     in
     (* We union-ize all symbols together, starting with the first one that has already been added *)
-    let pc, _last_sym =
+    let union_find, _last_sym =
       List.fold_left
-        (fun (pc, last_sym) sym ->
-          (Union_find.union ~merge:Smtml.Expr.Set.union last_sym sym pc, sym) )
-        (pc, hd) tl
+        (fun (union_find, last_sym) sym ->
+          ( Union_find.union ~merge:Smtml.Expr.Set.union last_sym sym union_find
+          , sym ) )
+        (union_find, hd) tl
     in
-    pc
+    { union_find }
   | [] ->
     (* It means smtml did not properly simplified an expression! *)
     assert false
@@ -36,18 +42,18 @@ let add (condition : Smtml.Typed.Bool.t) (pc : t) : t =
   Smtml.Expr.Set.fold add_one splitted_condition pc
 
 (* Get all sub conditions of the path condition as a list of independent sets of constraints. *)
-let slice pc = Union_find.explode pc
+let slice (pc : t) = Union_find.explode pc.union_find
 
 (* Return the set of constraints from [pc] that are relevant for [sym]. *)
-let slice_on_symbol (sym : Smtml.Symbol.t) pc : Smtml.Expr.Set.t =
-  match Union_find.find_opt sym pc with
+let slice_on_symbol (sym : Smtml.Symbol.t) (pc : t) : Smtml.Expr.Set.t =
+  match Union_find.find_opt sym pc.union_find with
   | Some s -> s
   | None ->
     (* if there is a symbol, it should have been added to the union-find structure before, otherwise it means `add` has not been called properly before *)
     assert false
 
 (* Return the set of constraints from [pc] that are relevant for [c]. *)
-let slice_on_condition (c : Smtml.Typed.Bool.t) pc : Smtml.Expr.Set.t =
+let slice_on_condition (c : Smtml.Typed.Bool.t) (pc : t) : Smtml.Expr.Set.t =
   match Smtml.Typed.get_symbols [ c ] with
   | sym0 :: _tl ->
     (* we need only the first symbol as all the others should have been merged with it *)
